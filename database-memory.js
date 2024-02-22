ecc61af33b72d8f89b7f67db76e16b474fb594b7
Vitor Madalosso <vitorconnected@gmail.com>
2024-02-21 20:23:09 -0500

    API Node.js + Postgres - Done

--- database-memory.js
+++ database-memory.js
@@ -3,8 +3,24 @@ import { randomUUID } from "node:crypto"
 export class DatabaseMemory {
   #videos = new Map()
 
-  list() {
-    return this.#videos.values()
+  list(search) {
+    return Array.from(this.#videos.entries())
+      .map((videoArray) => {
+        const id = videoArray[0]
+        const data = videoArray[1]
+
+        return {
+          id,
+          ...data,
+        }
+      })
+      .filter((video) => {
+        if (search) {
+          return video.title.includes(search)
+        }
+
+        return true
+      })
   }
 
   create(video) {
